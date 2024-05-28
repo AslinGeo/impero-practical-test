40e7e2f9e7db9a89812f8adcf0222ce47b73fb19
AslinGeo <jeojeo169@gmail.com>
2024-05-28 19:31:25 +0530

    initial setup

---
+++ lib/app/pages/home/view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/pages/home/controller.dart';
+import 'package:get/get.dart';
+
+class HomeView extends GetResponsiveView<HomeController> {
+  HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
